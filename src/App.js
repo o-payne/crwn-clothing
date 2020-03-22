1d7e01adbcf495fbabbed76f611676e655ec7a55
Owen <owen.payne42@gmail.com>
2020-03-22 16:35:50 -0400

    updating to redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import ShopPage from './Pages/Shop/shop.component'
 import { connect } from 'react-redux'
 
 import HomePage from "./Pages/Homepage/homepage.component"
 import Header from './Components/Header/header.component'
-import SignInAndSignUp from './Pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up'
+import SignInAndSignUpPage from './Pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up'
 import { auth, createUserProfileDocument } from './Firebase/firebase.utils'
 import { setCurrentUser } from './Redux/User/user.actions'
 
@@ -49,16 +49,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route exact path="/signin" render={() =>
+            this.props.currentUser ? (
+              <Redirect to='/' />
+            ) : (
+                <SignInAndSignUpPage />
+              )
+          }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
