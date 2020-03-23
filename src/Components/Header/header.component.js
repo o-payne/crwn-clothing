16ff94bafb46ae542cca5237aeeb09bf33acaa7d
Owen <owen.payne42@gmail.com>
2020-03-22 22:18:19 -0400

    added cart icon and cart dropdown components to application, also implemented cart reducer

--- src/Components/Header/header.component.js
+++ src/Components/Header/header.component.js
@@ -6,32 +6,40 @@ import { auth } from '../../Firebase/firebase.utils'
 
 import { ReactComponent as Logo } from '../../Assets/logo.svg'
 import './header.styles.scss'
+import CartIcon from '../cart-icon/cart-icon'
+import CartDropdown from '../Cart-Dropdown/cart-dropdown'
 
-const Header = ({currentUser}) => 
-(
-    <div className='header'>
-        <Link className='logo-container' to="/" >
-            <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
+const Header = ({ currentUser, hidden }) =>
+    (
+        <div className='header'>
+            <Link className='logo-container' to="/" >
+                <Logo className='logo' />
             </Link>
-            <Link className='option' to='/contact'>
-                CONTACT
+            <div className='options'>
+                <Link className='option' to='/shop'>
+                    SHOP
             </Link>
+                <Link className='option' to='/contact'>
+                    CONTACT
+            </Link>
+                {
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className='option' to='/signin'>SIGN IN</Link>
+                }
+                <CartIcon />
+            </div>
             {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                hidden ? null :
+                    <CartDropdown />
             }
         </div>
-    </div>
-)
+    )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser: currentUser,
+    hidden: hidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/Redux/root-reducer.js
+++ src/Redux/root-reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 
 import userReducer from './User/user-reducer'
+import cartReducer from './Cart/cart.reducer'
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
+
 })
